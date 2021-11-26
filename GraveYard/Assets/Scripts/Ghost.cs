307e4175595b35cb8ff37202a3f6f7fd0a9cef4b
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2021-11-26 15:43:09 -0800

    Update Ghost.cs

--- GraveYard/Assets/Scripts/Ghost.cs
+++ GraveYard/Assets/Scripts/Ghost.cs
@@ -6,7 +6,6 @@ namespace GraveYard
 {
     public class Ghost : MonoBehaviour
     {
-        private Transform[] ghostParts;
         private Vector3 startPosition;
 
         private float floatDistance;
@@ -25,9 +24,6 @@ namespace GraveYard
             length = floatDistance * 2;
             // The low position of the ping-pong
             bottomFloor = startPosition.y + floatDistance + transform.localScale.y;
-
-            ghostParts = new Transform[3];
-            ghostParts = GetComponentsInChildren<Transform>();
         }
 
         // Update is called once per frame
